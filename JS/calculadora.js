358d1f4e250167509ea1d5c86e2880b79c43498b
Luiz <luizhlimagomes28@gmail.com>
2021-12-05 20:20:49 -0300

    calculadora finalizada! Com todas as funções trabalhando

--- JS/calculadora.js
+++ JS/calculadora.js
@@ -1,15 +1,42 @@
 
 // concatena os numeros no visor
 function insert (num) {
-
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById ('resultado').innerHTML = numero + num;
+    
+    var numero = document.getElementById('resultado').innerHTML; 
+     document.getElementById ('resultado').innerHTML = numero + num;
+  
 }
 
 
 //limpa o visor de resultado
-function clear(){
+function limpar(){
+    
+    return document.getElementById('resultado').innerHTML = '';
 
-    document.getElementById('resultado').innerHTML = " ";
+}
+
+
+function voltar(){
+
+ var ApagarResultado = document.getElementById('resultado').innerHTML;
+document.getElementById('resultado').innerHTML = ApagarResultado.substring(0, ApagarResultado.length -1);
+}
+    
+
+
+function calcular (){
+
+var numCalculo = document.getElementById('resultado').innerHTML;
+if (numCalculo) {
+
+    document.getElementById('resultado').innerHTML = eval(numCalculo);
+
+} else{
+
+    document.getElementById('resultado').innerHTML = "nada para calcular";
+    
+
+}
+
 
 }
